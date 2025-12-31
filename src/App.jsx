78097313efcb9ebbf65a6461e7f9318cb1aefd65
Ayan Sahil <ayans8557@gmail.com>
2025-12-31 15:01:19 +0530

    api implemented axios working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState } from "react";
+import axios from "axios";
 
 const App = () => {
+
+const [allData, setAllData] = useState([])
+
+  const harryData = async () => {
+    const response = await axios.get("https://potterapi-fedeperin.vercel.app/en/characters");
+   setAllData(response.data)
+  };
+
+  
+
   return (
-    <div>App</div>
-  )
+   <div className="min-h-screen bg-black text-3xl text-amber-700">
+
+
+
+      <button onClick={harryData}>GET DATA</button>
+      {allData.map(function(elem,idx){
+        return <h1 key={idx}>{elem.fullName}</h1>; 
+      })}
+    </div>
+  );
 }
 
-export default App
+
+export default App;
